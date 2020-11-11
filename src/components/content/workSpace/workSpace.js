09d0e1e6b199e2168ed8669393bf1583c4e192c1
missclickk <70086295+missclickk@users.noreply.github.com>
2020-11-11 06:20:40 +0300

    swipe work

--- src/components/content/workSpace/workSpace.js
+++ src/components/content/workSpace/workSpace.js
@@ -1,16 +1,20 @@
-import { wait } from '@testing-library/react';
 import React, { useEffect, useState } from 'react'
 import styles from './workSpace.module.css'
-
+import "./opacity.css"
 const WorkSpace = () => {
     const [mouseDownOnSpace, setMouseOnDown] = useState(false);
-    let  pointerCor=null;
-    let flag=true;
+    let pointerCor = null;
+    let flag = true;
+    let firstObjCor = null;
+    let opacityClassNumber=0;
+    let range=60;
+    let leftRight="left";
     let [firstPointerCor, setFirstPointerCor] = useState(null);
+ 
     useEffect(() => {
         if (mouseDownOnSpace) {
+           
             document.addEventListener("mousemove", changeCoordinate);
-            document.addEventListener("mouseup", stopChangingCoordinate);
             window.getSelection().removeAllRanges();
         }
     }, [mouseDownOnSpace]);
@@ -18,41 +22,58 @@ const WorkSpace = () => {
 
     const stopChangingCoordinate = () => {
         setMouseOnDown(false);
+        const target = document.getElementsByClassName(styles.whiteSpace)[0];
+        if(target.classList[1]==="opacity7")
+        target.classList.remove(`${ target.classList[1]}`);
+        target.style.left=`${firstObjCor}px`;  
         document.removeEventListener("mousemove", changeCoordinate);
         document.removeEventListener("mouseup", stopChangingCoordinate);
     }
 
-
-
     const changeCoordinate = (event) => {
         const cor = event.pageX;
-        if(flag){
-            pointerCor=firstPointerCor;
-            flag=false;
+        if (flag) {
+            pointerCor = firstPointerCor;
+            flag = false;
+            firstObjCor = document.getElementsByClassName(styles.whiteSpace)[0].getBoundingClientRect().left;
         }
         const target = document.getElementsByClassName(styles.whiteSpace)[0];
         if (mouseDownOnSpace) {
-            let objCoordinate = Math.floor(target.getBoundingClientRect().left + cor -pointerCor);
+            let objCoordinate = Math.floor(target.getBoundingClientRect().left + cor - pointerCor);
             target.style.left = `${objCoordinate}px`;
-           pointerCor=cor;
-
+            if(cor<pointerCor)
+            leftRight="left";
+           if(cor>pointerCor)
+            leftRight="right";
+            pointerCor = cor;
         }
 
+        if ((target.getBoundingClientRect().left>firstObjCor + range &&   leftRight==="right" &&  firstObjCor<target.getBoundingClientRect().left )||(target.getBoundingClientRect().left<firstObjCor - range &&   leftRight==="left"  &&  firstObjCor>target.getBoundingClientRect().left)) {
+            target.classList.remove(`opacity${opacityClassNumber-1}`);
+            target.classList.add(`opacity${opacityClassNumber}`);
+            opacityClassNumber++;
+            range+=60;
+            if(target.classList[1]===`opacity7`)
+            stopChangingCoordinate();
+        }
+        if ((target.getBoundingClientRect().left<firstObjCor + range && leftRight==="left"  &&  firstObjCor<target.getBoundingClientRect().left)||(target.getBoundingClientRect().left>firstObjCor - range && leftRight==="right"  &&  firstObjCor>target.getBoundingClientRect().left)) {
+            target.classList.remove(`opacity${opacityClassNumber+1}`);
+            target.classList.add(`opacity${opacityClassNumber}`);
+            opacityClassNumber--;
+            range-=60;
+        }
     }
-    /*
-target.style.left=`${target.getBoundingClientRect().left+100}px`;
-target.style.top=`${top}px`;
-document.removeEventListener("mouseup", changeCoordinate);  
-*/
 
-    const click = (event) => {
+    const mouseDown = (event) => {
         setFirstPointerCor(event.pageX);
         setMouseOnDown(true);
     }
 
     return (
         <div className={styles.workSpace}>
-            <div className={styles.whiteSpace} onMouseDown={click} />
+            <div className={styles.area}>
+            <div className={styles.whiteSpace} onMouseDown={mouseDown} onMouseUp={stopChangingCoordinate} />
+        </div>
         </div>
     )
 
